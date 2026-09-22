9fb0a33e50561b2f9e4dc038b97086bd2e481ae5
hoangnm-ndm <hoangnm.ndm@gmail.com>
2026-09-22 16:20:47 +0700

    Day 04

---
+++ Day04/ex01.js
@@ -0,0 +1,26 @@
+console.log("Chuc mung Trung!");
+
+const objectA = {
+  name: "Hoang",
+  age: 35,
+};
+
+console.log(objectA);
+
+const myDocument = {
+  doctyle: "",
+  html: {
+    body: {
+      header: {},
+      main: {
+        a: {
+          children: "Click me!",
+          href: "google.com",
+        },
+      },
+      footer: {
+        children: "noi dung footer",
+      },
+    },
+  },
+};
